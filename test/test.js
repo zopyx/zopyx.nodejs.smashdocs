b34d000bc593beb3842871aebe2cb5c8ec8a74fa
Andreas Jung <info@zopyx.com>
2017-03-13 19:33:43 +0100

    updated

--- test/test.js
+++ test/test.js
@@ -7,7 +7,7 @@ describe('SmashdocsTests', function() {
     var client_id = process.env.SMASHDOCS_CLIENT_ID;
     var client_key = process.env.SMASHDOCS_CLIENT_KEY;
     var partner_url = process.env.SMASHDOCS_PARTNER_URL;
-    var debug =  process.env.SMASHDOCS_DEBUG;
+    var debug = process.env.SMASHDOCS_DEBUG;
     var group_id = 'sample-grp';
     var sd = new SMASHDOCs(partner_url, client_id, client_key, group_id, debug);
         
@@ -19,6 +19,11 @@ describe('SmashdocsTests', function() {
         'userId': 'schlumpf'
     };
 
+    function new_doc() {
+        var result = sd.new_document('doc title', 'doc description', 'editor', 'draft', user_data);
+        return result['documentId'];
+    }
+
     describe('SMASHDOCs API tests', function() {
         it('new_document()', function() {
             var result = sd.new_document('doc title', 'doc description', 'editor', 'draft', user_data);
@@ -26,27 +31,26 @@ describe('SmashdocsTests', function() {
         });
 
         it('document_info()', function() {
-            var result = sd.new_document('doc title', 'doc description', 'editor', 'draft', user_data);
-            var info = sd.document_info(result['documentId']);
+            var document_id = new_doc();
+            var info = sd.document_info(document_id);
             chai.expect(info).to.have.any.keys('title', 'description', 'status');            
         });
 
         it('delete_document()', function() {
-            var result = sd.new_document('doc title', 'doc description', 'editor', 'draft', user_data);
-            sd.delete_document(result['documentId']);
-            chai.expect(function() {sd.delete_document(result['documentId'])}).to.throw(Error);
+            var document_id = new_doc();
+            sd.delete_document(document_id);
+            chai.expect(function() {sd.delete_document(document_id)}).to.throw(Error);
         });
 
         it('archive_document()', function() {
-            var result = sd.new_document('doc title', 'doc description', 'editor', 'draft', user_data);
-            sd.archive_document(result['documentId']);
-            sd.unarchive_document(result['documentId']);
-            chai.expect(function() {sd.unarchive_document(result['documentId'])}).to.throw(Error);
+            var document_id = new_doc();
+            sd.archive_document(document_id);
+            sd.unarchive_document(document_id);
+            chai.expect(function() {sd.unarchive_document(document_id)}).to.throw(Error);
         });
 
         it('review_document()', function() {
-            var result = sd.new_document('doc title', 'doc description', 'editor', 'draft', user_data);
-            var document_id = result['documentId'];
+            var document_id = new_doc();
 
             var document_info = sd.document_info(document_id);
             chai.expect(document_info['status']).to.equal('draft');
@@ -59,8 +63,7 @@ describe('SmashdocsTests', function() {
 
         it('export_document_sdxml()', function() {
             this.timeout(10000);
-            var result = sd.new_document('doc title', 'doc description', 'editor', 'draft', user_data);
-            var document_id = result['documentId'];
+            var document_id = new_doc();
             var templates = sd.list_templates();
             var fn = sd.export_document(document_id, user_id='ajung', format='sdxml', template_id=templates[0]['id']);
             chai.expect(fn).to.endsWith('.zip');
@@ -68,8 +71,7 @@ describe('SmashdocsTests', function() {
 
         it('export_document_html()', function() {
             this.timeout(10000);
-            var result = sd.new_document('doc title', 'doc description', 'editor', 'draft', user_data);
-            var document_id = result['documentId'];
+            var document_id = new_doc();
             var templates = sd.list_templates();
             var fn = sd.export_document(document_id, user_id='ajung', format='html', template_id=templates[0]['id']);
             chai.expect(fn).to.endsWith('.zip');
@@ -77,8 +79,7 @@ describe('SmashdocsTests', function() {
 
         it('export_document_docx()', function(done) {
             this.timeout(10000);
-            var result = sd.new_document('doc title', 'doc description', 'editor', 'draft', user_data);
-            var document_id = result['documentId'];
+            var document_id = new_doc();
             var templates = sd.list_templates();
             var fn = sd.export_document(document_id, user_id='ajung', format='docx', template_id=templates[0]['id']);
             chai.expect(fn).to.endsWith('.docx');
