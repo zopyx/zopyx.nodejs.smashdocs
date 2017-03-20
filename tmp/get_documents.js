b6cb631efa1cd1d443f346aa6eec6ad7009433c5
Andreas Jung <info@zopyx.com>
2017-03-20 16:04:14 +0900

    added

---
+++ tmp/get_documents.js
@@ -0,0 +1,12 @@
+var api = require('./api');
+
+var client_id = process.env.SMASHDOCS_CLIENT_ID;
+var client_key = process.env.SMASHDOCS_CLIENT_KEY;
+var partner_url = process.env.SMASHDOCS_PARTNER_URL;
+var debug = process.env.SMASHDOCS_DEBUG;
+var group_id = 'sample-grp';
+var sd = new api(partner_url, client_id, client_key, group_id, debug);
+
+console.log(sd.get_documents('', 'ajung'));
+
+console.log(sd.get_documents('foo', ''));
