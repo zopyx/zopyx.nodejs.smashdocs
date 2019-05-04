9780300a25356595f51c10630126c714af48153e
Andreas Jung <info@zopyx.com>
2019-05-04 08:37:17 -0400

    updated

--- test/test.js
+++ test/test.js
@@ -5,6 +5,7 @@ var joinPath = require('path.join');
 
 var TIMEOUT = 60000;
 var TEST_USER_ID = 'schlumpf';
+var RETRIES = 3;
 
 
 describe('SmashdocsTests', function() {
@@ -38,24 +39,28 @@ describe('SmashdocsTests', function() {
     describe('SMASHDOCs API tests', function() {
         it('new_document()', function() {
             this.timeout(TIMEOUT);
+            this.retries(RETRIES);
             var result = sd.new_document('doc title', 'doc description', 'editor', 'draft', user_data);
             chai.expect(result).to.have.any.keys('documentId', 'documentAccessLink');            
         });
 
         it('upload_docx()', function() {
             this.timeout(TIMEOUT);
+            this.retries(RETRIES);
             var fn = joinPath(__dirname, 'test.docx');
             var result = sd.upload_document(fn, 'doc title', 'doc description', 'editor', 'draft', user_data);
         });
 
         it('upload_sdxml()', function() {
             this.timeout(TIMEOUT);
+            this.retries(RETRIES);
             var fn = joinPath(__dirname, 'test_sdxml_large.zip');
             var result = sd.upload_document(fn, 'doc title', 'doc description', 'editor', 'draft', user_data);
         });
 
         it('document_info()', function() {
             this.timeout(TIMEOUT);
+            this.retries(RETRIES);
             var document_id = new_doc();
             var info = sd.document_info(document_id);
             chai.expect(info).to.have.any.keys('title', 'description', 'status');            
@@ -63,6 +68,7 @@ describe('SmashdocsTests', function() {
 
         it('delete_document()', function() {
             this.timeout(TIMEOUT);
+            this.retries(RETRIES);
             var document_id = new_doc();
             sd.delete_document(document_id);
             chai.expect(function() {sd.delete_document(document_id)}).to.throw(Error);
@@ -70,6 +76,7 @@ describe('SmashdocsTests', function() {
 
         it('archive_document()', function() {
             this.timeout(TIMEOUT);
+            this.retries(RETRIES);
             var document_id = new_doc();
             sd.archive_document(document_id);
             sd.unarchive_document(document_id);
@@ -78,6 +85,7 @@ describe('SmashdocsTests', function() {
 
         it('review_document()', function() {
             this.timeout(TIMEOUT);
+            this.retries(RETRIES);
             var document_id = new_doc(false);
 
             var document_info = sd.document_info(document_id);
@@ -91,6 +99,7 @@ describe('SmashdocsTests', function() {
 
         it('export_document_sdxml()', function() {
             this.timeout(TIMEOUT);
+            this.retries(RETRIES);
             var document_id = new_doc();
             var templates = sd.list_templates();
             var fn = sd.export_document(document_id, user_id=TEST_USER_ID, format='sdxml', template_id=templates[0]['id']);
@@ -99,6 +108,7 @@ describe('SmashdocsTests', function() {
 
         it('export_document_html()', function() {
             this.timeout(TIMEOUT);
+            this.retries(RETRIES);
             var document_id = new_doc();
             var templates = sd.list_templates();
             var fn = sd.export_document(document_id, user_id=TEST_USER_ID, format='html', template_id=templates[0]['id']);
@@ -107,6 +117,7 @@ describe('SmashdocsTests', function() {
 
         it('export_document_docx()', function(done) {
             this.timeout(TIMEOUT);
+            this.retries(RETRIES);
             var document_id = new_doc();
             var templates = sd.list_templates();
             var fn = sd.export_document(document_id, user_id=TEST_USER_ID, format='docx', template_id=templates[0]['id']);
@@ -116,6 +127,7 @@ describe('SmashdocsTests', function() {
 
         it('update_metadata()', function() {
             this.timeout(TIMEOUT);
+            this.retries(RETRIES);
             var document_id = new_doc();
             sd.update_metadata(document_id, {title: 'bar'});
             var di = sd.document_info(document_id);
@@ -124,12 +136,14 @@ describe('SmashdocsTests', function() {
 
         it('duplicate_document()', function() {
             this.timeout(TIMEOUT);
+            this.retries(RETRIES);
             var document_id = new_doc(false);
             var result = sd.duplicate_document(document_id, title="new title", description="new description", creator_id=TEST_USER_ID);
         });
 
         it('get_documents()', function() {
             this.timeout(TIMEOUT);
+            this.retries(RETRIES);
             var document_id = new_doc();
             var result = sd.get_documents('', 'schlumpf');
             var result = result.filter(function(item) {return item['id'] == document_id});
@@ -139,11 +153,13 @@ describe('SmashdocsTests', function() {
 
         it('unseen_count()', function() {
             this.timeout(TIMEOUT);
+            this.retries(RETRIES);
             var result = sd.unseen_count('schlumpf');
         });
 
         it('list_unseen_changes()', function() {
             this.timeout(TIMEOUT);
+            this.retries(RETRIES);
             var result = sd.list_unseen_changes('schlumpf');
         });
     });
